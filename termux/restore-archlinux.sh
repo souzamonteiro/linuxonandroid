a7edd3567c2b16d5df6d1ea6adec9dfc3b6a78b1
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2024-10-04 19:05:21 -0300

    Update restore-archlinux.sh

--- termux/restore-archlinux.sh
+++ termux/restore-archlinux.sh
@@ -1,5 +1,7 @@
 #/bin/sh
 
+chmod -R 777 /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/archlinux
+
 rm -rf  /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/archlinux
 
 tar xzf archlinux.tar.gz -C /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/
