30e216eefdb63348dda424bb0bd0a7d6f4446110
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2024-10-07 18:22:54 -0300

    Add files via upload

--- termux/Debian.sh
+++ termux/Debian.sh
@@ -10,7 +10,9 @@ sleep 3
 export DISPLAY=:0
 termux-x11 :0 &
 
-virgl_test_server_android &
+MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles &
+
+#virgl_test_server_android &
 
 proot-distro login debian --user user --shared-tmp -- bash -c "export DISPLAY=:0 PULSE_SERVER=tcp:127.0.0.1; dbus-launch --exit-with-session startxfce4"
 
