88694c5cb9490329fe1a8a6bb33c3d815dd892f1
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2024-10-08 17:16:18 -0300

    Add files via upload

--- termux/Debian.sh
+++ termux/Debian.sh
@@ -7,11 +7,13 @@ am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity
 
 sleep 3
 
-virgl_test_server_android --angle-gl &
+virgl_test_server_android --angle-vulkan &
+#virgl_test_server_android --angle-gl &
+#virgl_test_server_android &
 
 export DISPLAY=:0
 export XDG_RUNTIME_DIR=${TMPDIR}
 termux-x11 :0 -ac &
 
-proot-distro login debian --user user --shared-tmp -- bash -c "export DISPLAY=:0 PULSE_SERVER=tcp:127.0.0.1; dbus-launch --exit-with-session startxfce4"
+proot-distro login debian --user user --shared-tmp --no-sysvipc -- bash -c "export DISPLAY=:0 PULSE_SERVER=tcp:127.0.0.1; dbus-launch --exit-with-session startxfce4"
 
