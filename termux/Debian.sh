47ba3f6ccd57f3fc821090afbdb1742485ae83a0
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2024-10-07 23:56:39 -0300

    Add files via upload

--- termux/Debian.sh
+++ termux/Debian.sh
@@ -7,10 +7,18 @@ am start --user 0 -n com.termux.x11/com.termux.x11.MainActivity
 
 sleep 3
 
-export DISPLAY=:0
-termux-x11 :0 &
+virgl_test_server_android --angle-gl &
 
-MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles &
+export DISPLAY=:0
+export XDG_RUNTIME_DIR=${TMPDIR}
+termux-x11 :0 -ac &
+
+#proot-distro login debian --user user --shared-tmp -- bash -c "export DISPLAY=:0 GALLIUM_DRIVER=virpipe MESA_GL_VERSION_OVERRIDE=4.6COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 dbus-launch --exit-with-session startxfce4 &"
+
+#export DISPLAY=:0
+#termux-x11 :0 &
+
+#MESA_NO_ERROR=1 MESA_GL_VERSION_OVERRIDE=4.3COMPAT MESA_GLES_VERSION_OVERRIDE=3.2 GALLIUM_DRIVER=zink ZINK_DESCRIPTORS=lazy virgl_test_server --use-egl-surfaceless --use-gles &
 
 #virgl_test_server_android &
 
